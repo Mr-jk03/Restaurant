73c0ca5435c8580f8ae4289c4f86928771f18bf4
Mr-jk03 <119925917+Mr-jk03@users.noreply.github.com>
2022-12-11 18:55:17 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+let menu = document.querySelector('#menu-bars');   /*trả về phần tử đầu tiên là phần tử con của phần tử mà nó được gọi ra khớp với nhóm bộ chọn được chỉ định */
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');   /*classList.toggle('ADD') để thêm class “ADD” vào thẻ<h2>*/ 
+    navbar.classList.toggle('active');
+}
+
+
+window.onscroll = () =>{         /*Sự kiên cuộn window.sonscroll */
+    
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    
+    loop:true,
+  });
